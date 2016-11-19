ba7ebe2477752db5b194abb8052ad7a0ddaf597a
paulius-git <paulius.micikevicius@gmail.com>
2016-11-19 10:13:50 -0800

    layer that reports maximum point-wise percent-difference between 2 blobs

---
+++ include/caffe/layers/maxpercentdiff_layer.hpp
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <vector>
+
+#include "caffe/blob.hpp"
+#include "caffe/layer.hpp"
+#include "caffe/proto/caffe.pb.h"
+
+namespace caffe {
+
+/**
+ * @brief Compute the maximum point-wise difference-percentage between two input
+ *   blobs:
+ *  bottom[0]: computed values
+ *  bototm[1]: target values
+ */
+
+template <typename Dtype>
+class MaxPercentDiffLayer : public Layer<Dtype> {
+ public:
+  explicit MaxPercentDiffLayer(const LayerParameter& param)
+      : Layer<Dtype>(param) {}
+  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+
+  virtual inline const char* type() const { return "AMaxDiff"; }
+  virtual inline int ExactNumBottomBlobs() const { return 2; }
+  virtual inline int ExactNumTopBlobs() const { return 1; }
+
+ protected:
+  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+
+  // Not implemented as this layer isn't intended for loss computation.
+  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom) {
+    for (int i = 0; i < propagate_down.size(); ++i) {
+      if (propagate_down[i]) { NOT_IMPLEMENTED; }
+    }
+  }
+};
+
+}  // namespace caffe
